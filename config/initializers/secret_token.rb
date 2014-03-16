e77fa8bd6415d25df2076aace84f7a8074236a3a
moonandfox <rachael@moonandfox.com>
2014-03-16 14:24:11 -0700

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RachApp::Application.config.secret_token = '455e398e877848daa70ec6e62a0efe01ad41fff9b64637e8239947708d32efb7e450580429415908eed6d73ab2e5063293bdebadfccbc84d7f127a2c8d2402d0'
